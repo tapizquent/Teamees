98a85975c19cac1d095562db5a02bf72a40fba29
Jose Tapizquent <josetapizq@gmail.com>
2017-09-10 13:25:09 -0400

    First commit

---
+++ Teamees/MainVC.swift
@@ -0,0 +1,26 @@
+//
+//  MainVC.swift
+//  Teamees
+//
+//  Created by Jose Tapizquent on 9/10/17.
+//  Copyright © 2017 Jose Tapizquent. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        collectionView?.backgroundColor = UIColor.white
+        
+
+        
+    }
+
+}
+
